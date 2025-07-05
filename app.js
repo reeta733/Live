2b97352d816ebac85d1387d69f1626ff26349a53
reeta733 <reetasiya@gmail.com>
2025-07-05 19:16:59 +0530

    changes in env file

--- app.js
+++ app.js
@@ -21,12 +21,17 @@
 
 import express from 'express';
 import allRoutes from './routes/allRoutes.js';
-import ownerProfileRoutes from './routes/OwnerProfileRoutes.js'; // ✅ Add this
+import ownerProfileRoutes from './routes/OwnerProfileRoutes.js';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
 import path from 'path';
 
-const app = express();
+const app = express(); 
+
+// This code for deployment ---start---
+const root = path.join(path.resolve(), 'dist');
+app.use(express.static(root));
+// This code for deployment ---end---
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -34,8 +39,12 @@ app.use(cors());
 app.use(fileUpload());
 app.use(express.static(path.resolve() + "/assets"));
 
-app.use(allRoutes); // Your existing routes
-app.use("/api/v1/owner-profile", ownerProfileRoutes); // ✅ This line fixes the problem
+app.use(allRoutes);
+app.use("/api/v1/owner-profile", ownerProfileRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: path.join(path.resolve(), 'dist') }  );
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
